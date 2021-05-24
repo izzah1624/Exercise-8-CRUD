78ec4582a99e8e62176417b8f2b03a3171cb4ba1
izzah1624 <61498548+izzah1624@users.noreply.github.com>
2021-05-24 10:27:03 +0800

    Add files via upload

---
+++ web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+//Route::resource('cats', App\Http\Controllers\catController) 
+Route::get('/about', 'App\Http\Controllers\catController@about');
+Route::get('/team', 'App\Http\Controllers\catController@team');
+Route::get('/services', 'App\Http\Controllers\catController@services'); 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
